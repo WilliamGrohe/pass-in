dd269c62503d248b15bfd3cd6828ce98ff13b524
WilliamGrohe <william.grohe@gmail.com>
2024-04-05 00:39:00 -0300

    Add dayjs for relative time display in attendee list;
    * Incorporate dayjs for relative time formatting in the attendee list
    * Modify AttendeeList component to use dayjs for relative time display in createdAt and checkedInAt fields
    * Add pagination to AttendeeList component
    * Adjust package.json to include dayjs as a dependency
    * Update package-lock.json due to the new dependency version

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -9,18 +9,43 @@ import {
   Search,
 } from "lucide-react";
 
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/pt-br"
+
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { attendees } from "../data/attendees";
 
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
 
 export function AttendeeList() {
   const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+
+  const totalPages = Math.ceil(attendees.length / 10)
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value)
+  }
+
+  function goToFirstPage(){
+    setPage(1)
+  }
+
+  function goToPreviousPage(){
+    setPage(page - 1)
+  }
+  
+  function goToNextPage(){
+    setPage(page + 1)
+  }
+
+  function goToLastPage(){
+    setPage(totalPages)
   }
 
   return (
@@ -57,7 +82,7 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {attendees.map((attendee) => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
               <tr key={attendee.id} className="border-b border-white/10 hover:bg-white/5">
                 <TableCell>
@@ -75,8 +100,8 @@ export function AttendeeList() {
                     <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>{attendee.createdAt.toISOString()}</TableCell>
-                <TableCell>{attendee.checkedInAt.toISOString()}</TableCell>
+                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
@@ -88,22 +113,22 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell colSpan={3}>Mostrando 10 de {attendees.length} itens</TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>Página {page} de {totalPages}</span>
 
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
