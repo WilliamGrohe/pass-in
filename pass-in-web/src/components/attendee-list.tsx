f8b773a33245e3ebce3c3f7dddcb21857dbb1408
WilliamGrohe <william.grohe@gmail.com>
2024-04-10 15:50:53 -0300

    Persisting URL state with params of page and search

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -30,8 +30,25 @@ interface Attendee {
 }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState(()=>{
+    const url = new URL(window.location.toString())
+
+    if(url.searchParams.has('search')){
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+
+  const [page, setPage] = useState(()=>{
+    const url = new URL(window.location.toString())
+
+    if(url.searchParams.has('page')){
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
 
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
@@ -46,6 +63,24 @@ export function AttendeeList() {
     url.searchParams.set('query', search)
   }
 
+  function setCurrentSearch(search: string){
+    const url = new URL(window.location.toString())
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, "", url)
+
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number){
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+    window.history.pushState({}, "", url)
+
+    setPage(page)
+  }
+
   useEffect(() =>{
     fetch(url)
       .then(response => response.json())
@@ -57,24 +92,24 @@ export function AttendeeList() {
   }, [page, search])
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
-    setPage(1)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage(){
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToPreviousPage(){
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
   
   function goToNextPage(){
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToLastPage(){
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   return (
@@ -86,6 +121,7 @@ export function AttendeeList() {
           <Search className="size-4 text-emerald-300" />
           <input
             onChange={onSearchInputChanged}
+            value={search}
             type="text"
             className="bg-transparent flex-1 outline-none border-none p-0 text-sm cursor-pointer focus:ring-0"
             placeholder="Buscar participante..."
