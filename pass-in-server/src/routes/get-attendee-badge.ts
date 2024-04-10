3b16abbaa9dbcdf83ddadcde71f176f34a7fb161
WilliamGrohe <william.grohe@gmail.com>
2024-04-10 00:39:43 -0300

    create route for get badge from an attendee

--- pass-in-server/src/routes/get-attendee-badge.ts
+++ pass-in-server/src/routes/get-attendee-badge.ts
@@ -11,7 +11,16 @@ export async function getAttendeeBadge(app: FastifyInstance){
         params: z.object({
           attendeeId: z.string().transform(Number),
         }),
-        response: {}
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            })
+          })
+        }
       }
     }, async (request, reply) => {
       const { attendeeId } = request.params
@@ -33,6 +42,17 @@ export async function getAttendeeBadge(app: FastifyInstance){
         throw new Error('Participante não encontrado.')
       }
 
-      return reply.send({ attendee })
+      const baseURL = `${request.protocol}://${request.hostname}`
+
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+      return reply.send({ 
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString()
+        }
+       })
     })
 }
